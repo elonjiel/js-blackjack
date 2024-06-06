5c2ffe32371e44d5469611e7489410f16f50f6e4
Elon Jiel <elonudemy@gmail.com>
2024-06-06 18:58:20 +0800

    Added More Functions

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard] // array - ordered list of items
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+let player = {
+    name: "Elon Jiel",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+ 
 function startGame() {
+    isAlive = true
+    hasBlackJack = false
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard] 
+    sum = firstCard + secondCard
     renderGame()
 }
 function renderGame() {
 
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for ( let i = 0; i < cards.length; i++ ){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
 
     if (sum <= 20) {
@@ -31,7 +59,11 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 6
-    sum += card
-    renderGame()
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+    
 }
